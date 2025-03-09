30873135b98a86495a5b9a06f02af2e199f43a4a
iDuqueZ <ivandariduque@gmail.com>
2025-03-09 16:12:08 -0500

    Añadir dependencia de Flowbite y ajustar estilos y contenido en la página de inicio

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+    darkMode: false, // Desactiva el modo oscuro
+    content: [
+      "./src/**/*.{astro,html,js,jsx,ts,tsx}", // Ajusta las rutas según tu estructura
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
