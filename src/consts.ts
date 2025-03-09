df92e68482974713b3055646ad10499e693a9ea9
iDuqueZ <ivandariduque@gmail.com>
2025-03-09 10:59:40 -0500

    Inicio

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'SIMTRAEMPUVALLE';
+export const SITE_DESCRIPTION = 'Sindicato de Base que propende por los derechos de sus afiliados y afiliadas, en el marco de la legalidad y la justicia social, dentro de la universidad del valle';
